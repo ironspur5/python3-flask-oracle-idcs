d8855300af46ca1c110240b57b8cedba6d84f625
Patrick Guha <ironspur5@users.noreply.github.com>
2020-05-07 15:38:17 -0500

    Security fixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import IdcsClient
 import json
 
 app = Flask(__name__)
+
 # secret key is needed for session
 app.secret_key = 'secret'
 
@@ -11,40 +12,24 @@ app.secret_key = 'secret'
 # Definition of the /auth route
 @app.route('/auth', methods=['POST', 'GET'])
 def auth():
-    print("----------------- def auth() ---------------")
+
     # Loading the configurations
-    options = getOptions()
-    print("config.json file = %s" % options)
+    options = getoptions()
+
     # Authentication Manager loaded with the configurations
     am = IdcsClient.AuthenticationManager(options)
+
     '''
     Using Authentication Manager to generate the Authorization Code URL, passing the
     application's callback URL as parameter, along with code value and code parameter
     '''
     url = am.getAuthorizationCodeUrl(options["redirectURL"], options["scope"], "1234", "code")
 
-    # print(getAccessToken(options['BaseUrl'], options['ClientId'], options['ClientSecret']))
-
     # Redirecting the browser to the Oracle Identity Cloud Service Authorization URL.
     return redirect(url, code=302)
 
-
-def getAccessToken(URL, clientId, clientSecret):
-    data = {
-        'grant_type': 'client_credentials',
-        'scope': 'urn:opc:idm:__myscopes__'
-    }
-    # Request for auth token
-    response = requests.post(URL + '/oauth2/v1/token', data=data, verify=False, auth=(clientId, clientSecret))
-    # Parses response into JSON format
-    res = response.json()
-    # Parse access token
-    access_token = res['access_token']
-    return access_token
-
-
 # Function used to load the configurations from the config.json file
-def getOptions():
+def getoptions():
     fo = open("config.json", "r")
     config = fo.read()
     options = json.loads(config)
@@ -54,17 +39,17 @@ def getOptions():
 # Definition of the /logout route
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
-    code = (session.get("code", None))
     id_token = (session.get("id_token", None))
-    print(code)
-    print(id_token)
 
-    options = getOptions()
+    options = getoptions()
 
     url = options["BaseUrl"]
     url += options["logoutSufix"]
     url += '?post_logout_redirect_uri=http%3A//localhost%3A8000&id_token_hint='
     url += id_token
+
+    # clears Flask client-side session (also works on refresh)
+    session.clear()
 
     # Redirect to Oracle Identity Cloud Service logout URL
     return redirect(url, code=302)
@@ -77,28 +62,22 @@ def login():
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
+
+    # first call IDCS API to get id_token; should return 400 status if not authenticated
+    # if statement to kick you back to login if status is 400
+    # if authenticated, status 200 allows app to render protected html
+
+    options = getoptions()
+
+    # 'code' is authorization code which is needed to get id_token
+    # uses flask.request library (different from Python requests library)
     session['code'] = request.args.get('code')
-
-    # Todo: check if code actually is IDCS code and not spoofed fake one (call IDCS API to validate)
-
-    '''
-    this check here is so that people cant just 
-    go to website like http://www.app.com/home effectively skipping the login authentication route;
-    '''
-    if session['code'] == 'None':
-        return render_template('login.html')
-
-    print(session['code'])
-    options = getOptions()
 
     data = {
         'grant_type': 'authorization_code',
         'code': session['code'],
         'redirect_uri': options['redirectURL']
     }
-
-    print(data['code'])
-    print(data['redirect_uri'])
 
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
@@ -108,12 +87,12 @@ def home():
                              auth=(options['ClientId'], options['ClientSecret']))
 
     session['id_token'] = response.json().get("id_token")
-    print(response.json().get("id_token"))
+
+    if str(response.status_code) != "200":
+        return render_template('login.html')
 
     return render_template('home.html')
 
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
-
-
